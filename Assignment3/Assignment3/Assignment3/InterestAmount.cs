f39a75c36acf4abc514dfe6e6bc68c4a06c19e4e
Saikumar Kakumanu <s.kakumanu@zensar.com>
2022-04-23 11:53:46 +0530

    Csharp Assignment3

---
+++ Assignment3/Assignment3/Assignment3/InterestAmount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class InterestAmount
+    {
+        double rate;
+    
+        public void loanCalculator(double loanamt)
+        {
+            Console.WriteLine("Enter rate of interest");
+            rate = Convert.ToDouble(Console.ReadLine());
+            double interest = (loanamt * rate) / 100;
+            Console.WriteLine("Rate of interest for "+rate+"%  is : "+interest);
+        }
+    }
+}
---
+++ Assignment3/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // student marks program
+            Student student = new Student(1234, "abcd", "Btech", "1stsem", "cse");
+            student.displaydata();
+            student.displayresult();
+
+            // car program
+
+            Car car = new Car(9999, "BMW", "SUV");
+            car.displaycardetails();
+            Car car2 = new Car(500000);
+
+            // Loan amount calculator
+
+            InterestAmount ia = new InterestAmount();
+            ia.loanCalculator(200000);
+            Console.ReadLine();
+        }
+    }
+}
