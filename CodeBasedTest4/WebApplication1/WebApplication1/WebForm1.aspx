9bc931694e737e1810244c2b4f6a55f0d0d28c9f
Saikumar Kakumanu <s.kakumanu@zensar.com>
2022-06-16 18:09:20 +0530

    Codebase Test 4

---
+++ CodeBasedTest4/WebApplication1/WebApplication1/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" Height="350px" Width="636px" ImageAlign="Right" />
+            Select Your Car :
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Height="16px" Width="205px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            &nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+           <p> Check The Price : <asp:Button ID="Button1" runat="server" Text="Price" OnClick="Button1_Click" />
+               &nbsp;
+               <asp:TextBox ID="TextBox1" runat="server" Height="21px" Width="140px"></asp:TextBox>
+               </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CodeBasedTest4/WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] str = new string[] { "Select", "Audi", "BMW", "Jeep", "Tata" };
+                for(int i=0; i<str.Length; i++)
+                {
+                    DropDownList1.Items.Add(str[i]);
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = DropDownList1.Text;
+            Image1.ImageUrl = "~/Pics/" + str + ".jfif";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text=DropDownList1.SelectedIndex.ToString();
+            if (DropDownList1.Text == "Audi")
+            {
+                TextBox1.Text = "Rs. 50Lacks";
+            }
+            else if(DropDownList1.Text == "BMW")
+            {
+                TextBox1.Text = "Rs. 1cr";
+            }
+            else if (DropDownList1.Text == "Jeep")
+            {
+                TextBox1.Text = "Rs.20,00,000";
+            }
+            else if (DropDownList1.Text == "Tata")
+            {
+                TextBox1.Text = "Rs. 10,00,000";
+            }
+            else
+            {
+                TextBox1.Text = " ";
+            }
+        }
+    }
+}
